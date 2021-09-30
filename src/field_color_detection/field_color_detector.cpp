a57ea000a5ac155aea47a0b78dac0f763c60fdaa
PedroBonorin0 <pepebonorinobraga@gmail.com>
2021-09-30 12:18:18 -0300

    Limitando a pintura na deteccao do campo

--- src/field_color_detection/field_color_detector.cpp
+++ src/field_color_detection/field_color_detector.cpp
@@ -3,63 +3,93 @@
 using namespace std;
 using namespace cv;
 
-#define UPPERGREENCHROMATICITYTHRESHOLDLINES 0.43 // Valor para usar na deteccao das linhas
-#define UPPERGREENCHROMATICITYTHRESHOLDFIELD 0.46 // Valor usado para deteccao do campo
+#define UPPERGREENCHROMATICITYTHRESHOLD 0.43
 #define REDCHROMATICITYTHRESHOLD0 0.37
 #define LOWERGREENCHROMATICITYTHRESHOLD 0.4
 #define BLUECHROMATICITYTHRESHOLD 0.38
 // #define BLUECHROMATICITYTHRESHOLD 0.47
 
 // Constructor
-Field_color_detector::Field_color_detector(){}
+Field_color_detector::Field_color_detector() {}
 
 // Destructor
-Field_color_detector::~Field_color_detector(){}
+Field_color_detector::~Field_color_detector() {}
 
-float Field_color_detector::getChromaticity(uchar channelColor, Vec3b color){
+float Field_color_detector::getChromaticity(uchar channelColor, Vec3b color)
+{
     return channelColor / (float) (color[0] + color[1] + color[2]);
 }
 
-void Field_color_detector::paintField(Mat inputImg){
+void Field_color_detector::paintField(Mat inputImg)
+{   
+    int cont; // Counts not green pixels in sequence 
+
     Mat segmentedImg = inputImg.clone();
-    for (size_t i = 0; i < inputImg.cols; i++)
-    {
-        for (size_t j = inputImg.rows; j > 0; j--)
-        {
+    bool paint = false;
+    for (size_t i = 0; i < inputImg.cols; i++){
+        cont = 0;
+        for (size_t j = inputImg.rows; j > 0; j--){
             auto pixelColor = inputImg.at<Vec3b>(j, i);
-            if(isField(pixelColor)){
+            if (isField(pixelColor)) { 
                 segmentedImg.at<Vec3b>(j, i) = {255, 0, 100};
+                cont = 0;
+            } else {
+                cont++;
+            }
+
+            if(cont >= 10) { // Verificar valor
+                break;
+                // int aux; // Count greens in sequence
+                
+                // for(int z = j; z > 0; z--){
+                //     pixelColor = inputImg.at<Vec3b>(j, i);
+                //     if (isField(pixelColor)){
+                //         aux++;
+                //     } else {
+                //         aux = 0;
+                //     }
+
+                //     if(aux > 5){
+                //         paint = true;
+                //         break;
+                //     }
+                // }
+                // if(!paint){
+                //     cont = 0;
+                //     break;
+                // }
             }
         }
     }
-    imshow("vai", segmentedImg);
+    imshow("Painted Field", segmentedImg);
 }
 
 bool Field_color_detector::isField(Vec3b pixel){
-    // if pixel is field colored returns true
-    if(getChromaticity(pixel[0], pixel) < BLUECHROMATICITYTHRESHOLD &&
-       getChromaticity(pixel[1], pixel) >= UPPERGREENCHROMATICITYTHRESHOLDFIELD && 
-       getChromaticity(pixel[2], pixel) < REDCHROMATICITYTHRESHOLD0){
-        return true;
+    if (getChromaticity(pixel[0], pixel) < BLUECHROMATICITYTHRESHOLD &&
+        getChromaticity(pixel[2], pixel) < REDCHROMATICITYTHRESHOLD0 &&
+        getChromaticity(pixel[1], pixel) >= 0.38){ // Valor melhor?
+         return true;
     }
-    return false;
 
-    // for (size_t i = 0; i < inputImg.cols; i++)
-    // {
-    //     for (size_t j = 0; j < inputImg.rows; j++)
-    //     {
-    //         auto pixelColor = inputImg.at<Vec3b>(j, i);
-    //         if (getChromaticity(pixelColor[0], pixelColor) > BLUECHROMATICITYTHRESHOLD) 
-    //             continue;
-    //         if (getChromaticity(pixelColor[2], pixelColor) > REDCHROMATICITYTHRESHOLD0) 
-    //             continue;
-    //         if (getChromaticity(pixelColor[1], pixelColor) >= LOWERGREENCHROMATICITYTHRESHOLD){
-    //             // if(getChromaticity(pixelColor[1], pixelColor) <= UPPERGREENCHROMATICITYTHRESHOLD)
-    //             segmentedImg.at<Vec3b>(j, i) = {255, 0, 100};
-    //         }
-    //     }
-    // }
+    return false;
 }
+
+// for (size_t i = 0; i < inputImg.cols; i++)
+// {
+//     for (size_t j = 0; j < inputImg.rows; j++)
+//     {
+//         auto pixelColor = inputImg.at<Vec3b>(j, i);
+//         if (getChromaticity(pixelColor[0], pixelColor) > BLUECHROMATICITYTHRESHOLD)
+//             continue;
+//         if (getChromaticity(pixelColor[2], pixelColor) > REDCHROMATICITYTHRESHOLD0)
+//             continue;
+//         if (getChromaticity(pixelColor[1], pixelColor) >= LOWERGREENCHROMATICITYTHRESHOLD){
+//             // if(getChromaticity(pixelColor[1], pixelColor) <= UPPERGREENCHROMATICITYTHRESHOLD)
+//             segmentedImg.at<Vec3b>(j, i) = {255, 0, 100};
+//         }
+//     }
+// }
+// }
 
 // VideoCapture cap = VideoCapture(argv[1]);
 // VideoWriter videoWritter = VideoWriter();
